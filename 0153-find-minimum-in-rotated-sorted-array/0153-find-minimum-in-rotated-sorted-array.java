0be667631d323009aa28bd2b777a49c1fd377c38
Nachiappan-RM <65343089+Nachiappan-RM@users.noreply.github.com>
2025-06-18 19:47:46 -0700

    Time: 0 ms (100%), Space: 42.1 MB (44.46%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,12 +1,37 @@
 class Solution {
-    public int findMin(int[] nums) {
-        int min = nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]<nums[i-1]){
-                min = nums[i];
-                break;
+    // public int findMin(int[] nums) {
+    //     int min = nums[0];
+    //     for(int i=1;i<nums.length;i++){
+    //         if(nums[i]<nums[i-1]){
+    //             min = nums[i];
+    //             break;
+    //         }
+    //     }
+    //     return min;
+    // }
+
+    // -------REVISION--------
+    /**
+    Try to find the place index that actually decreases -> To find start and and end 
+    Do binary from there
+    ->O(n + log n)
+    
+    But Thinking of how I can do that in O(log n)
+    Thinking of Binary search
+
+     */
+    public int findMin(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] > nums[end]){
+                start = mid+1;
+            }else{
+                end = mid;
             }
         }
-        return min;
+        return nums[start];
     }
 }
