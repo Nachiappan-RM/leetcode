ff52e465bb510f4996d04b00b03fcca19b9df4f2
Nachiappan-RM <65343089+Nachiappan-RM@users.noreply.github.com>
2025-07-27 11:11:41 -0700

    Time: 1 ms (98.92%), Space: 45 MB (41.11%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> resultList = new ArrayList<>();
@@ -29,7 +30,7 @@ class Solution {
         return result;
     }
 }
-
+*/
 /**
 
 Find an interval that has the start
@@ -42,3 +43,39 @@ if(start<=currend) move forward
 get the end of the index get the max(currend, traversed end)
 
  */
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0, n = intervals.length;
+        int newStart = newInterval[0], newEnd = newInterval[1];
+
+        //Phase 1: add all intervals that end before the new interval starts
+        while(i<n && intervals[i][1] < newStart) {
+            result.add(new int[]{intervals[i][0], intervals[i][1]});
+            i++;
+        }
+
+        //Phase 2: merge all the overlapping intervals
+        while(i<n && intervals[i][0] <= newEnd) {
+            newStart = Math.min(newStart, intervals[i][0]);
+            newEnd = Math.max(newEnd, intervals[i][1]);
+            i++;
+        }
+
+        result.add(new int[]{newStart, newEnd});
+
+        //Phase 3: append the rest of the intervals
+        while( i < n) {
+            result.add(new int[]{intervals[i][0], intervals[i][1]});
+            i++;
+        }
+
+        //converting it back to a 2D array
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+//Running through testcases like below,
+// [[1,3],[6,9]]
+
