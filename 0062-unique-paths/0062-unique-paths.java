dc3cb9671244610a5256b09381ec296ecf91f945
Nachiappan-RM <65343089+Nachiappan-RM@users.noreply.github.com>
2025-08-29 19:48:53 -0500

    Time: 0 ms (100%), Space: 40.3 MB (89.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int uniquePaths(int m, int n) {
         if(m == 1 || n== 1){
@@ -19,5 +20,34 @@ class Solution {
         }
 
         return matrix[m-1][n-1];
+    }
+}
+*/
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        dp[m-1][n-1] = 1;
+
+        for (int i = 0; i < m; i++) {
+            dp[i][n-1] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[m-1][j] = 1;
+        }
+        
+        for(int i=m-2; i>=0; i--) {
+            for(int j=n-2; j>=0; j--) {
+                if(i+1 < m && j+1 < n) {
+                    dp[i][j] = dp[i+1][j] +dp[i][j+1] ;
+                }else if(i+1<m) {
+                    dp[i][j] = dp[i+1][j];
+                }else if(j+1<n){
+                    dp[i][j] = dp[i][j+1];
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
